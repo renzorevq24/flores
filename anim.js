e137515ec6d382551f1f4ab2b4db6ae613f169dd
renzorevq24 <u201717085@upc.edu.pe>
2024-09-21 11:24:27 -0500

    Update anim.js
    ...

--- anim.js
+++ anim.js
@@ -8,7 +8,7 @@ function es_11_11 () {
   let ahora = new Date(); 
   let horas = ahora.getHours(); 
   let minutos = ahora.getMinutes(); 
-  
+  return true
   if ((horas === 11 || horas === 23) && minutos >= 11 && minutos <= 16) {
       return true; 
   } else {
